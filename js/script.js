2720e7b96e4ba4b0729026c1ed3e9cbb4e7d5000
Kristijan <kskrlec@racunarstvo.hr>
2023-06-24 20:34:07 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Getting the scroll amount on the window, used for the sticky header
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $(".sticky").addClass("stickyadd");
+    } else {
+      $(".sticky").removeClass("stickyadd");
+    }
+  });
+
+  var typed = new Typed(".element", {
+    strings: ["Kristijan Škrlec", "a Developer", "a Photographer", "a BodyBuilder"],
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000,
+  });
+});
