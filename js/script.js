51d64200ae71cf9990cb4a7de8df40ae7bd40d2d
Kristijan <kskrlec@racunarstvo.hr>
2023-06-28 18:55:15 +0200

    Finalized the project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-  //ADDING THE PRELOADER ANIMATION
+  //Page Preloader Animation
   $(window).on("load", function () {
     $(".preloader").addClass("complete");
   });
 
+  //Sticky Header, Enlarged and Always Visible
   $(window).on("scroll", function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
@@ -13,7 +14,7 @@ $(document).ready(function () {
     }
   });
 
-  //ANIMATING THE TEXR
+  //Typewriter Animation Header
   var typed = new Typed(".element", {
     strings: [
       "Kristijan Škrlec",
@@ -29,7 +30,7 @@ $(document).ready(function () {
     startDelay: 1000,
   });
 
-  //ANIMATING THE PROGRESS BARS
+  //Animating the Progress Bars
   var waypoint = new Waypoint({
     element: document.getElementById("exp-id"),
     handler: function () {
@@ -43,11 +44,12 @@ $(document).ready(function () {
     offset: "90%",
   });
 
+  //Photo Gallery
   var filterizd = $(".filter-container").filterizr({
     animationDuration: 0.5,
   });
 
-  //OWL CAROUSEL FUNCTION
+  //Owl Carousel Functionality
   $(".owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
@@ -56,12 +58,12 @@ $(document).ready(function () {
     animateIn: "fadeInRight",
   });
 
-  //SMOOTH SCROLL
+  //Enabling the Smooth Scroll
   $("a").smoothScroll({
     speed: 2000,
   });
 
-  // adding fadeInUp animation to child of div with class .way-col
+  //Adding fadeInUp Animations to Children of .way-col
   var $child = $(".way-fade-up").children();
   $child.each(function () {
     var self = $(this);
@@ -95,213 +97,61 @@ $(document).ready(function () {
     );
   });
 
-  /*  USER LOGIN AND REGISTRATION */
-  $("#login-button").click(handleLogin);
-  $("#register-button").click(handleRegister);
+  /*  USER SIGN UP  */
+  // Handle registration form submission
+  $("#myModal").submit(function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    var usernameData = $("#floatingInput").val();
+    var password = $("#floatingPassword").val();
 
-  $(".message-box").hide();
+    //Perform login validation here
+    if (usernameData === "") {
+      alert("Please enter your email.");
+      return;
+    }
 
-  // Submit event for the login form
-  $("form.log-form").submit(function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+    if (password === "") {
+      alert("Please enter your password.");
+      return;
+    }
 
-    // Get the username and password from the input fields
-    var username = $("#username").val();
-    var password = $("#password").val();
+    // Send AJAX request to the server for verification
+    var registrationData = {
+      username: usernameData,
+      password: password,
+    };
 
-    // Send a POST request to the login API endpoint with the credentials
-    $.ajax({
-      url: "https://www.fulek.com/data/api/user/login",
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-      success: function (response) {
-        console.log("Success function called");
-        console.log(response); // Check the response for error message
-
-        if (response.statusCode === 404) {
-          // Display an error message in the message box
-          console.log("invalid");
-          $(".message-box")
-            .html("Invalid credentials!")
-            .addClass("error")
-            .addClass("show")
-            .delay(2000)
-            .fadeOut();
-          $(".message-box").addClass("unsuccess").css("display", "block");
-          $(".message-box")
-            .addClass("red-box")
-            .css("background-color", "#f55d90");
-          $(".curriculum").css("display", "block");
-        } else {
-          // Display a success message in the message box and close the login form
-          var token = response.data.token;
-          localStorage.length = 0;
-          sessionStorage.setItem("token", token);
-          sessionStorage.setItem("username", username);
-          console.log("valid");
-          $(".message-box")
-            .addClass("blue-box")
-            .css("background-color", "#00adb5");
-          $(".message-box")
-            .html("Login successful!")
-            .removeClass("error")
-            .removeClass("red-box")
-            .addClass("blue-box")
-            .addClass("show") // Add the "show" class
-            .delay(1000)
-            .fadeOut(function () {
-              $("form")[0].reset(); // Remove the register button
-              $("#login-form").fadeOut("slow"); // Close the login form slowly
-            });
-          // Clear the input fields
-          $(".login-navbar").remove(); // Remove the login button
-          $(".signup-navbar").remove();
-          // Set the login status to true
-          loggedIn = true;
-          // Add the logout button to the navbar
-          $("#menu").append(
-            '<li class="logout-navbar"><a href="#" id="logout-button" >Log out</a></li>'
-          );
-          $(".message-box").addClass("success").css("display", "block"); // Add the "success" class and set the display to "block"
-          // Re-add click event listener to the login button
-          $("#login-button").click(function () {
-            $("#login-form").toggle("slow");
-            if (loggedIn) {
-              // Show the login form if user is already logged in
-              $("#login-form").show();
-            }
-          });
-          $(".curriculum").addClass("show-cur").css("display", "block");
-        }
-      },
-
-      error: function (xhr, status, error) {
-        console.log("Error function called");
-        console.log(xhr);
-        console.log(status);
-        console.log(error);
-        console.log("Invalid credentials");
-        // Display an error message in the message box
-        $(".message-box")
-          .html("Invalid credentials!")
-          .addClass("error")
-          .addClass("show")
-          .delay(2000)
-          .fadeOut();
-      },
-    });
-  });
-  $(".reg-form").submit(function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Get the username, email and password from the input fields
-    var reg_username = $("#reg-username").val();
-
-    var reg_password = $("#reg-password").val();
-
-    // Send a POST request to the register API endpoint with the credentials
+    // Send AJAX request to the server for registration
     $.ajax({
       url: "https://www.fulek.com/data/api/user/register",
       type: "POST",
       contentType: "application/json",
-      data: JSON.stringify({
-        username: reg_username,
-        password: reg_password,
-      }),
+      data: JSON.stringify(registrationData),
       success: function (response) {
-        console.log("Success function register called");
-        console.log(response); // Check the response for error message
-
-        if (response.statusCode === 400) {
-          // Display an error message in the message box
-          console.log("invalid");
-          $(".message-box")
-            .html("User exists!")
-            .removeClass("blue-box")
-            .addClass("error")
-            .addClass("show")
-            .delay(2000)
-            .fadeOut();
-          $(".message-box").addClass("unsuccess").css("display", "block");
-          $(".message-box")
-            .addClass("red-box")
-            .css("background-color", "#f55d90");
-        } else {
-          // Display a success message in the message box and close the login form
-          console.log("valid");
-          $(".message-box")
-            .addClass("blue-box")
-            .css("background-color", "#00adb5");
-          $(".message-box")
-            .html("Registration successful!")
-            .removeClass("error")
-            .removeClass("red-box")
-            .addClass("blue-box")
-            .addClass("show") // Add the "show" class
-            .delay(1000)
-            .fadeOut(function () {
-              $("form")[0].reset(); // Remove the register button
-              $("#register-form").fadeOut("slow"); // Close the register form slowly
-            });
-
-          $(".message-box").addClass("success").css("display", "block");
-          registerFormActive = false;
+        // Check the response from the server
+        if (response.statusCode === 200) {
+          // Registration successful
+          alert("Registration successful!");
+          console.log(response);
+          $('#myModal').modal('hide');
+          $('#signupBtn').html("Log Out");
+        }
+        else if (response.statusCode === 400) {
+          alert("User exists, Logging In");
+          var usernameData = $("#floatingInput").val("");
+          var password = $("#floatingPassword").val("");
+          console.log(response);
+          $('#myModal').modal('hide');
+          $('#signupBtn').html("Log Out");
         }
       },
-
       error: function (xhr, status, error) {
-        console.log("Error function called");
-        console.log(xhr);
-        console.log(status);
-        console.log(error);
-
-        // Display an error message in the message box
-        $(".message-box")
-          .html("Registration failed!")
-          .addClass("error")
-          .addClass("show")
-          .delay(2000)
-          .fadeOut();
+        // Handle AJAX request error
+        alert(
+          "An error occurred while processing your request. Please try again later."
+        );
+        console.log(response);
       },
     });
-  });
-  // Add click event listener to the cancel button
-  $(".button.cancel").click(function () {
-    // Use toggle method with "slow" duration to hide the register form
-    $("#register-form").toggle("slow");
-    // Set registerFormActive variable to false
-    registerFormActive = false;
-  });
-
-  $(document).on("click", "#logout-button", function () {
-    // Set the login status to false
-    loggedIn = false;
-    loginFormActive = false;
-    sessionStorage.removeItem("username");
-    sessionStorage.removeItem("token");
-
-    //$(".curriculum").css("display", "none");
-
-    // Remove the logout button from the navbar
-    $("#logout-button").remove();
-    window.location.href = "/html/index.html";
-    checkLoggedIn();
-    // Add the login and register buttons back to the navbar
-    $("#menu").append(
-      '<li class="nav-item right-nav-item login-navbar"><button id="login-button">Login</button></li>'
-    );
-    document
-      .getElementById("login-button")
-      .addEventListener("click", handleLogin);
-    $("#menu").append(
-      '<li class="nav-item signup-navbar"><button id = "register-button" class="logs">Register</button></li>'
-    );
-    document
-      .getElementById("register-button")
-      .addEventListener("click", handleRegister);
   });
 });
